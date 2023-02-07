4708840a8e50d94e6a966699f02ccd78541c8d7c
Sameer1103 <124495346+Sameer1103@users.noreply.github.com>
2023-02-08 00:26:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var name1 = prompt("Enter Player 1's Name");
 var name2 = prompt("Enter Player 2's Name");
-if(!name1 || name1 === "")
+if (!name1 || name1 === "")
     name1 = "Player 1";
-if(!name2 || name2 === "")
+if (!name2 || name2 === "")
     name2 = "Player 2";
 $(".turn0").text(name1 + "'s Turn");
 $(".turn1").text(name2 + "'s Turn");
@@ -17,8 +17,8 @@ function showDice(n) {
 
 function convertToPoint(l, t) {
     var x, y;
-    x = Math.floor(l / 60) - 6;
-    y = (800 - t) / 60;
+    x = Math.floor(l + 45) / 60;
+    y = (555 - t) / 60;
     if (y % 2 == 0) return (y * 10) + x;
     return (y * 10) + 11 - x;
 }
@@ -29,8 +29,8 @@ function shiftToPoint(counter, e, i) {
     rows[i] = y + 1;
     if (y % 2 == 0) x = e - 10 * y;
     else x = 10 * y + 11 - e;
-    l = 60 * x + 360;
-    t = 800 - 60 * y;
+    l = 60 * x - 45;
+    t = 555 - 60 * y;
     $("." + counter).css("left", l + "px");
     $("." + counter).css("top", t + "px");
 }
@@ -43,29 +43,29 @@ function movec(num, i) {
     l = parseInt(l, 10);
     t = parseInt(t, 10);
     if (rows[i] % 2 === 1) {
-        d = 960 - l;
+        d = 555 - l;
         if (d >= num * 60)
-            $("."+c[i]).css("left", (l+num*60)+"px");
+            $("." + c[i]).css("left", (l + num * 60) + "px");
         else {
-            $("."+c[i]).css("left", "960px");
-            $("."+c[i]).css("top", (t-60)+"px");
+            $("." + c[i]).css("left", "555px");
+            $("." + c[i]).css("top", (t - 60) + "px");
             rows[i]++;
             d = num * 60 - d - 60;
             if (d > 0)
-                $("."+c[i]).css("left", (960-d)+"px");
+                $("." + c[i]).css("left", (555 - d) + "px");
         }
     }
     else if (rows[i] === 10) {
-        d = l - 420;
+        d = l - 15;
         if (d > num * 60)
-            $("."+c[i]).css("left", (l-num*60)+"px");
+            $("." + c[i]).css("left", (l - num * 60) + "px");
         else if (d < num * 60) {
-            $("."+c[i]).css("left", "420px");
+            $("." + c[i]).css("left", "15px");
             d = num * 60 - d;
-            $("."+c[i]).css("left", (420+d)+"px");
+            $("." + c[i]).css("left", (15 + d) + "px");
         }
         else {
-            $("."+c[i]).css("left", "420px");
+            $("." + c[i]).css("left", "15px");
             $(".board-img").before("<img src= 'Images/fireworks.webp' alt= 'fw-img' class= 'fw fw1'>");
             if (i === 1) {
                 $(".fw").removeClass("fw1");
@@ -73,44 +73,44 @@ function movec(num, i) {
             }
             $(".turn0").remove();
             $(".turn1").remove();
-            if(i === 0)
-            $(".win").text(name1 + " Wins");
+            if (i === 0)
+                $(".win").text(name1 + " Wins");
             else $(".win").text(name2 + " Wins");
             $("button").removeClass("btn");
             $("button").addClass("btn1");
-            $("button").html("<a href = 'game.html'>Restart</a>");
+            $("button").html("<a href = 'index.html'>Restart</a>");
             $("button a").css("text-decoration", "none");
             $("button a").css("color", "white");
             return;
         }
     }
     else {
-        d = l - 420;
+        d = l - 15;
         if (d >= num * 60)
-            $("."+c[i]).css("left", (l-num*60)+"px");
+            $("." + c[i]).css("left", (l - num * 60) + "px");
         else {
-            $("."+c[i]).css("left", "420px");
-            $("."+c[i]).css("top", (t-60)+"px");
+            $("." + c[i]).css("left", "15px");
+            $("." + c[i]).css("top", (t - 60) + "px");
             rows[i]++;
             d = num * 60 - d - 60;
             if (d > 0)
-                $("."+c[i]).css("left", (420+d)+"px");
+                $("." + c[i]).css("left", (15 + d) + "px");
         }
     }
-    setTimeout(function(){
-    l = $("."+c[i]).css("left");
-    t = $("."+c[i]).css("top");
-    l = parseInt(l,10);
-    t = parseInt(t,10);
-    console.log(l+" , "+t);
-    d = convertToPoint(l,t);
-    if(M.has(d)){
-        console.log(d);
-        e = M.get(d);
-        console.log(e);
-        shiftToPoint(c[i], e, i);
-    }
-    },1000);
+    setTimeout(function () {
+        l = $("." + c[i]).css("left");
+        t = $("." + c[i]).css("top");
+        l = parseInt(l, 10);
+        t = parseInt(t, 10);
+        console.log(l + " , " + t);
+        d = convertToPoint(l, t);
+        if (M.has(d)) {
+            console.log(d);
+            e = M.get(d);
+            console.log(e);
+            shiftToPoint(c[i], e, i);
+        }
+    }, 1000);
     setTimeout(function () {
         t1 = parseInt($("." + c[0]).css("top"), 10);
         t2 = parseInt($("." + c[1]).css("top"), 10);
@@ -130,7 +130,7 @@ function movec(num, i) {
 var c = ["redcounter", "bluecounter"], i = 0, num, M, l, t, e;
 M = new Map([[1, 38], [4, 14], [9, 31], [17, 7], [21, 42], [28, 84], [51, 67], [54, 34], [62, 19], [64, 60], [72, 91], [80, 99], [87, 36], [93, 73], [95, 75], [98, 79]]);
 var rows = [1, 1]; // Rows in which counters are present
-var t1,t2,l1,l2;
+var t1, t2, l1, l2;
 t1 = parseInt($("." + c[0]).css("top"), 10);
 t2 = parseInt($("." + c[1]).css("top"), 10);
 l1 = parseInt($("." + c[0]).css("left"), 10);
@@ -143,21 +143,21 @@ if (l1 === l2 && t1 === t2) {
     t = parseInt(t, 10);
     $("." + c[1]).css("top", (t - 15) + "px");
 }
-    $(".turn1").hide();
-    $(".btn").click(function () {
-        $(".board-img").before("<img src= 'Images/dicef.webp' alt= 'dice-thrown' class= 'dthrow'>");
-        setTimeout(function () { $(".dthrow").remove(); }, 2000);
-        num = Math.floor(Math.random() * 6) + 1;
-        setTimeout(showDice, 2000, num);
-        var t1 = parseInt($("." + c[0]).css("top"), 10), t2 = parseInt($("." + c[1]).css("top"), 10);
-        if (t1 - t2 === 30) {
-            $("." + c[0]).css("top", (t1 - 15) + "px");
-            $("." + c[1]).css("top", (t2 + 15) + "px");
-        }
-        setTimeout(movec, 3000, num, i);
-        if (num != 6) {
-            $(".turn" + i).hide();
-            i = (i + 1) % 2;
-            $(".turn" + i).show();
-        }
-    });
+$(".turn1").hide();
+$(".btn").click(function () {
+    $(".board-img").before("<img src= 'Images/dicef.webp' alt= 'dice-thrown' class= 'dthrow'>");
+    setTimeout(function () { $(".dthrow").remove(); }, 2000);
+    num = Math.floor(Math.random() * 6) + 1;
+    setTimeout(showDice, 2000, num);
+    var t1 = parseInt($("." + c[0]).css("top"), 10), t2 = parseInt($("." + c[1]).css("top"), 10);
+    if (t1 - t2 === 30) {
+        $("." + c[0]).css("top", (t1 - 15) + "px");
+        $("." + c[1]).css("top", (t2 + 15) + "px");
+    }
+    setTimeout(movec, 3000, num, i);
+    if (num != 6) {
+        $(".turn" + i).hide();
+        i = (i + 1) % 2;
+        $(".turn" + i).show();
+    }
+});
